90b1337d52f0612a849d68cd5e598f27438c3958
Brendan Stock <brendan@twopaperdolls.com>
2018-07-12 11:29:42 -0400

    fix formatting

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -3,23 +3,23 @@ import Book from './Book';
 
 class Bookshelf extends Component {
 
-render() {
+    render() {
 
-const { shelfInfo, books } = this.props;
+        const { shelfInfo, books } = this.props;
 
-return (
-<div className="bookshelf">
-<h2 className="bookshelf-title">{shelfInfo.name}</h2>
-<div className="bookshelf-books">
-<ol className="books-grid">
+        return (
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{shelfInfo.name}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
 
-{ books.map( (book) => <Book key={book.id} bookInfo={book} populateShelves={() => this.props.populateShelves()} /> ) }
+                        { books.map( (book) => <Book key={book.id} bookInfo={book} populateShelves={() => this.props.populateShelves()} /> ) }
 
-</ol>
-</div>
-</div>
-)
-}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
 
 }
 
