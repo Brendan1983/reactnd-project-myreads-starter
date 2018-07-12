494f6ac279670070d3fff3b2643a5f95f2526951
Brendan Stock <brendan@twopaperdolls.com>
2018-07-12 11:26:00 -0400

    fix indent on bookshelf

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -5,19 +5,19 @@ class Bookshelf extends Component {
 
 	render() {
 
-    const { shelfInfo, books } = this.props;
+	  const { shelfInfo, books } = this.props;
 
 		return (
 			<div className="bookshelf">
-        <h2 className="bookshelf-title">{shelfInfo.name}</h2>
-        <div className="bookshelf-books">
-          <ol className="books-grid">
-            
-          	{ books.map( (book) => <Book key={book.id} bookInfo={book} populateShelves={() => this.props.populateShelves()} /> ) }
-            
-          </ol>
-        </div>
-      </div>
+	      <h2 className="bookshelf-title">{shelfInfo.name}</h2>
+	      <div className="bookshelf-books">
+	        <ol className="books-grid">
+	          
+	        	{ books.map( (book) => <Book key={book.id} bookInfo={book} populateShelves={() => this.props.populateShelves()} /> ) }
+	          
+	        </ol>
+	      </div>
+	    </div>
 		)
 	}
 
